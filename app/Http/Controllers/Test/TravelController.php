256db155e6bff2f33ea9fcb5e67853ad06864bba
lijianguo <lijianguo@whzrt.com>
2018-12-17 20:26:19 +0800

    依赖三之依赖注入

--- app/Http/Controllers/Test/TravelController.php
+++ app/Http/Controllers/Test/TravelController.php
@@ -6,9 +6,14 @@ namespace App\Http\Controllers\Test;
 class TravelController
 {
     public $obj;
-    public function __construct()
+    /*public function __construct()
     {
         $this->obj = new CarController('北京','开汽车');
+    }*/
+
+    public function __construct()
+    {
+        $this->obj = (new FactoryController('CarController','南京','开汽车'))->choice();
     }
 
     public function go()
