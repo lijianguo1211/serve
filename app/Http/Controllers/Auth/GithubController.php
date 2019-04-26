c6c474396107980eb50c96302376ff5d12883fe6
lijianguo <lijianguo@whzrt.com>
2019-04-26 11:49:51 +0800

    github

--- app/Http/Controllers/Auth/GithubController.php
+++ app/Http/Controllers/Auth/GithubController.php
@@ -27,10 +27,40 @@ class GithubController extends Controller
 
     public function handleProviderCallback()
     {
-        $user = $this->socialite->driver('github')->user();
-        \Log::info(json_encode($user));
-        $data = [
+        $github = $this->socialite->driver('github')->user();
 
+        $data = [
+            'username' => $github->getUsername(),
+            'email'    => $github->getEmail(),
+            'phone'    => $this->getPhone(),
+            'validate_token' => str_random(24),
+            'is_validate' => 1,
+            'provider' => 'github',
+            'avatar' => $github->getAvatar(),
         ];
+    }
+
+    public function getPhone()
+    {
+        $str = '3456789';
+        $strlen = strlen($str);
+        $two = mt_rand(0,$strlen-1);
+        $oneStr = 1;
+        $str = str_shuffle($str);
+        $twoStr = $str[$two];
+        $strs = '0123456789';
+        $strsLen = strlen($strs);
+
+        $code = '';
+        for($i=0;$i<9;$i++) {
+
+            $e = mt_rand(0,$strsLen-1);
+            $strs = str_shuffle($strs);
+            $code .= $strs[$e];
+        }
+
+        $phone = $oneStr.$twoStr.$code;
+
+        return $phone;
     }
 }
